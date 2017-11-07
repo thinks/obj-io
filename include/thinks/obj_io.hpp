9b5298596986ee1ab774a271018103d8a5b08e7e
Tommy Hinks <tommy.hinks@gmail.com>
2017-11-07 23:01:34 +0100

    first commit

---
+++ include/thinks/obj_io.hpp
@@ -0,0 +1,43 @@
+// Copyright 2017 Tommy Hinks
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+#ifndef THINKS_OBJ_IO_HPP_INCLUDED
+#define THINKS_OBJ_IO_HPP_INCLUDED
+
+#include <ostream>
+#include <vector>
+
+namespace thinks {
+namespace obj_io {
+namespace detail {
+
+
+} // namespace detail
+
+std::ostream& Write(std::ostream& os)
+{
+  return os;
+}
+
+
+} // namespace obj_io
+} // namespace thinks
+
+#endif // THINKS_OBJ_IO_HPP_INCLUDED
